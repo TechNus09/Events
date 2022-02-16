b37266ce4c9fe96cb558ef3efae35f0e5ed2f349
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-02-16 15:11:56 +0100

    Update db_helper.py
    add checkT()

--- db_helper.py
+++ db_helper.py
@@ -8,7 +8,8 @@ db_host = os.environ.get("DB_HOST")
 db_port = os.environ.get("DB_PORT")
 db_name = os.environ.get("DB_NAME")
 #################################################################################
-def createdb():
+def createT():
+#hhhhhhjjjjjjj
     try:
         connection = psycopg2.connect(
                                       user=db_user,
@@ -38,6 +39,51 @@ def createdb():
             return True
         else:
             return false
+#################################################################################
+def checkT(table_name):
+    table = table_name
+    try:
+        connection = psycopg2.connect(
+                                      user=db_user,
+                                      password=db_pw,
+                                      host=db_host,
+                                      port=db_port,
+                                      database=db_name
+                                     )
+        cursor = connection.cursor()
+        # SQL query to check table exist
+        cursor.execute(f"SELECT EXISTS(SELECT relname 
+                                       FROM pg_class
+                                       WHERE relname = {table});")
+        
+        
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if not cursor.fetchone()[0]:
+            createT()
+            print(f"Table {table} Created")
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+            return True
+        else:
+            return false
+
+#################################################################################
+
+
+
+#################################################################################
+
+
+
+#################################################################################
+
+
+
+
+
 #################################################################################
 
 
